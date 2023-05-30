164e65b946ebd44195a45cd778505625f4216950
Margarita Bushlyakova <bushmargarita4@gmail.com>
2023-05-30 16:30:13 +0300

    add 3 tests in UserServiceTest, add postgresql library

--- test/by/fpmibsu/netside/service/UserServiceTest.java
+++ test/by/fpmibsu/netside/service/UserServiceTest.java
@@ -1,51 +1,55 @@
 package test.by.fpmibsu.netside.service;
 
 import org.testng.Assert;
-
+import org.testng.annotations.BeforeClass;
+import src.by.fpmibsu.netside.dao.DaoException;
+import src.by.fpmibsu.netside.entity.User;
+import src.service.UserService;
 
 
 public class UserServiceTest extends Assert {
-    /*UserService userService = new UserService();
-    @Test
-    public void testGeneratedStub () {}
 
-    @org.testng.annotations.BeforeMethod(enabled=false)
+    private UserService userService;
+    @BeforeClass
+    private void set() throws DaoException {
+        userService=new UserService();
+        if(userService.getUserByName("Margo")!=null){
+            userService.delete(userService.getUserByName("Margo"));}
+    }
+
+
+
+    @org.testng.annotations.BeforeMethod()
     public void setUp() throws DaoException {
-        boolean f=userService.createUser("Margo","11111", "bushmargarita4@gmail.com");
 
     }
 
-    @org.testng.annotations.AfterMethod(enabled=false)
+    @org.testng.annotations.AfterMethod()
     public void tearDown() throws DaoException {
-        boolean f=userService.delete(new User("Margo","11111", "bushmargarita4@gmail.com"));
 
     }
 
-    @org.testng.annotations.Test
-    public void testGetUserByName() throws DaoException {
-        Assert.assertEquals(userService.getUserByName("Margo"),
-                new User("Margo", "11111", "bushmargarita4@gmail.com")
-        );
-    }
-
-    @org.testng.annotations.Test
+    @org.testng.annotations.Test(priority =0)
     public void testCreateUser() throws DaoException {
-        Assert.assertFalse(userService.createUser("Margo", "11111", "bushmargarita4@gmail.com"));
+        assert(userService.createUser("Margo", "11111", "bushmargarita4@gmail.com"));
     }
 
-    @org.testng.annotations.Test
+    @org.testng.annotations.Test(priority = 1)
     public void testFindUserById() throws DaoException {
         Integer id =userService.getUserByName("Margo").getId();
-        Assert.assertEquals(userService.findUserById(id),
-                new User("Margo", "11111", "bushmargarita4@gmail.com")
-        );
-    }*/
-    @org.testng.annotations.Test
-    public void testFindUserById() {
-        Integer id =0;
-        Integer id2=0;
-        Assert.assertEquals(id,id2);
+        User actual=userService.findUserById(id);
+        User expected=new User(id,"Margo", "11111", "bushmargarita4@gmail.com");
+        assertEquals(actual.getLogin(), expected.getLogin());
+        assertEquals(actual.getEmail(),expected.getEmail());
+        assertEquals(actual.getPassword(),expected.getPassword());
     }
 
-
+    @org.testng.annotations.Test(priority = 3)
+    public void testGetUserByName() throws DaoException {
+        User actual=userService.getUserByName("Margo");
+        User expected=new User("Margo", "11111", "bushmargarita4@gmail.com");
+        assertEquals(actual.getLogin(), expected.getLogin());
+        assertEquals(actual.getEmail(),expected.getEmail());
+        assertEquals(actual.getPassword(),expected.getPassword());
+    }
 }
